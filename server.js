b8ed8c871632e32d71c2f1dac38aad5160bcfeee
Margaret Rahu <margaret.rahu@tptlive.ee>
2024-01-31 14:15:53 +0200

    jee

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const nunjucks = require('nunjucks');
 const app = express();
 const port = 3000;
 
+app.use(express.urlencoded({extended:true}))
+
 nunjucks.configure('views', {
     autoescape: true,
     express: app
@@ -20,6 +22,17 @@ app.get('/page2', (req, res) => {
 app.get('/form', (req, res) => {
   console.log(req.query);
   res.render('form.njk', req.query);
+});
+
+app.get('/circle', (req, res) => {
+  res.render('circle.njk', req.query);
+});
+
+app.post('/circle', (req, res) => {
+  let area = Math.PI * req.body.radius * req.body.radius;
+  let circumference = 2 * Math.PI * req.body.radius;
+  let volume = 4/3 * Math.PI * req.body.radius * req.body.radius * req.body.radius;
+  res.render("circleAnswer.njk", {r: req.body.radius, a: area, c: circumference, v: volume });
 });
 
 app.listen(port, () => {

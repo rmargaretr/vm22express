9ce09893e373bf93ccfc65b52bc2de27e46a651f
Margaret Rahu <margaret.rahu@tptlive.ee>
2024-02-07 15:31:29 +0200

    mees maas

--- server.js
+++ server.js
@@ -35,6 +35,9 @@ app.post('/circle', (req, res) => {
   res.render("circleAnswer.njk", {r: req.body.radius, a: area, c: circumference, v: volume });
 });
 
+const movieController = require('./src/movieController.js');
+app.use('/movies', movieController);
+
 app.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`);
 });

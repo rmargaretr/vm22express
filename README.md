5a64e479a0749e9d9fd36bed57929e82a8b015fd
rmargaretr <145546692+rmargaretr@users.noreply.github.com>
2024-04-24 20:52:45 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,21 +1,9 @@
-# vm22express
-learn express
+# vm22learnexpress
 
-vsc - console.log('hello terminal') -> terminal
--> terminal - node faili.nimi
-hello terminal
+## install
 
+`npm i`
 
-terminal - npm install express - andis package-lock.json ja node_modules
+## run
 
-
-vsc - fail main kataloogis ''.gitignore'' - git, pls igno seda faili
-faili sisse kirjuta failid (node_modules), mida tahad ignoda
-
-
-et ei peaks igakord crl+c uuendamiseks - terminal .. ''npx nodemon''
-package.jsonis  "main": "server.js", mitte index.html, et töötaks
-
-NPM START
-
-
+`npm start`
--- server.js
+++ server.js
@@ -14,21 +14,26 @@ app.use(session({
   saveUninitialized: false,
 }));
 
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({
+  extended:true
+}));
 
 const env = nunjucks.configure('views', {
     autoescape: true,
     express: app
 });
-app.use((req, res, next) =>{
+
+app.use((req, res, next) => {
   env.addGlobal('user', req.session.user);
+  env.addGlobal('errors', req.session.errors);
+  req.session.errors = null;
+  req.session.save();
   next();
 });
 
 app.get('/', (req, res) => {
   console.log(req.session.user);
   res.render('index.njk');
-  //console.log('somebody visited');
 });
 
 app.get('/page2', (req, res) => {
@@ -41,23 +46,21 @@ app.get('/form', (req, res) => {
 });
 
 app.get('/circle', (req, res) => {
-  res.render('circle.njk', req.query);
+  res.render('circle.njk');
 });
 
 app.post('/circle', (req, res) => {
-  let area = Math.PI * req.body.radius * req.body.radius;
-  let circumference = 2 * Math.PI * req.body.radius;
-  let volume = 4/3 * Math.PI * req.body.radius * req.body.radius * req.body.radius;
-  res.render("circleAnswer.njk", {r: req.body.radius, a: area, c: circumference, v: volume });
+    let area = Math.PI * req.body.radius * req.body.radius;
+    res.render('circleAnswer.njk', {r: req.body.radius, a: area});
 });
 
 const movieController = require('./src/movieController.js');
 app.use('/movies', movieController);
 
 app.get('/cookie', (req, res) => {
-  res.cookie('mycookie', 'kõrsikud', {maxAge: 1000*60*60*24*30});
-  if(!req.session.secretValue) {
-    req.session.secretValue = new Date();   
+  res.cookie('mycookie', 'cool cookie', {maxAge: 1000*60*60*24*356*200});
+  if(!req.session.secretValue){
+    req.session.secretValue = new Date();
   }
   res.send(req.session);
 });
